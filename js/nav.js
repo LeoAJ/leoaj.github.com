a20d826d65e074944ce8ef1e652463754730dc05
Leo <Leo0924@gmail.com>
2013-06-15 13:48:42 -0700

    add nav.js

---
+++ js/nav.js
@@ -0,0 +1,17 @@
+$(function() {
+    console.log('YA');
+    var header = $('header'),
+        pos = header.offset();
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > header.height() && header.hasClass('default')) {
+            header.fadeOut('fast', function() {
+                $(this).removeClass('default').addClass('color').fadeIn('fast');
+            });
+        } else if ($(this).scrollTop() <= pos.top && header.hasClass('color')) {
+            header.fadeOut('fast', function() {
+                $(this).removeClass('color').addClass('default').fadeIn('fast');
+            });
+        }
+    });
+});
