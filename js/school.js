49a9cb5079d1eceab6fc71f2ee590e67872a4314
Leo <Leo0924@gmail.com>
2013-06-15 14:02:15 -0700

    school project version 1.0

--- js/school.js
+++ js/school.js
@@ -1,18 +1,4 @@
-(function() {
-
-    $('a.single').fancybox();
-
-    $('.item').on('click', function() {
-        
-        var $this = $(this);
-        
-        $this.animate({
-            height: '300px'
-        });
-
-        $('.description').css({
-            display: 'block'
-        });
-    });
-
-})();
+$(function() {
+    // set picture gallery
+    $('.fancybox').fancybox();
+});
